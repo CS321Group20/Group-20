fd947dc67197edc4411a2e0d95c7f7042879de5a
arkanjumani <148118853+arkanjumani@users.noreply.github.com>
2023-11-30 18:16:17 -0500

    Update Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,3 @@
-package org.javafx;
 //Author: Arkan Jumani 
 public interface Person { //person interface that is shared with the Applicant class 
     public String getInfo(); //getInfo method displays information in one concise statement.
